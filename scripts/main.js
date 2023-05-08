415aba309ad8ee294e1bd7438613a9d08575f9c8
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-08 12:58:03 +0200

    add script scissors / paper btn events

--- scripts/main.js
+++ scripts/main.js
@@ -47,6 +47,86 @@ rockBtn.addEventListener("click", function(){
         resultContent.innerHTML = "Gagné !";
         playerScore = playerScore +1;
     }
+
+    console.log(playerScore);
+    console.log(computerScore);
+})
+
+paperBtn.addEventListener("click", function(){
+    let randomSign = Math.floor(Math.random() * signs.length);
+    let computerSign = signs[randomSign];
+
+    let resultContainer = document.createElement("div");
+    resultContainer.classList.add("results-container", "col-lg-6");
+    let resultContent = document.createElement("p");
+    let playerChooseElement = document.createElement("i");
+    playerChooseElement.classList.add("fa-solid", "fa-hand")
+    let computerChooseElement = document.createElement("i");
+    btnsRow.appendChild(resultContainer);
+    resultContainer.appendChild(resultContent);
+    resultContainer.appendChild(playerChooseElement);
+    resultContainer.appendChild(computerChooseElement);
+    
+    for (i = 0; i < btnsContainer.length; i++){
+        btnsContainer[i].style.display = "none";
+    }
+
+    if(computerSign == "Rock"){
+        computerChooseElement.classList.add("fa-sharp", "fa-solid", "fa-hand-back-fist");
+        resultContent.innerHTML = "Gagné !";
+        playerScore = playerScore +1;
+        
+    }
+
+    if(computerSign == "Paper"){
+        computerChooseElement.classList.add("fa-solid", "fa-hand");
+        resultContent.innerHTML = "Egalité !";
+    }
+
+    if(computerSign == "Scissors"){
+        computerChooseElement.classList.add("fa-solid", "fa-hand-scissors");
+        resultContent.innerHTML = "Perdu !";
+        computerScore = computerScore +1;
+    }
+})
+
+scissorsBtn.addEventListener("click", function(){
+    let randomSign = Math.floor(Math.random() * signs.length);
+    let computerSign = signs[randomSign];
+
+    let resultContainer = document.createElement("div");
+    resultContainer.classList.add("results-container", "col-lg-6");
+    let resultContent = document.createElement("p");
+    let playerChooseElement = document.createElement("i");
+    playerChooseElement.classList.add("fa-solid", "fa-hand-scissors")
+    let computerChooseElement = document.createElement("i");
+    btnsRow.appendChild(resultContainer);
+    resultContainer.appendChild(resultContent);
+    resultContainer.appendChild(playerChooseElement);
+    resultContainer.appendChild(computerChooseElement);
+    
+    for (i = 0; i < btnsContainer.length; i++){
+        btnsContainer[i].style.display = "none";
+    }
+
+    if(computerSign == "Rock"){
+        computerChooseElement.classList.add("fa-sharp", "fa-solid", "fa-hand-back-fist");
+        resultContent.innerHTML = "Perdu !";
+        computerScore = computerScore +1;
+        
+    }
+
+    if(computerSign == "Paper"){
+        computerChooseElement.classList.add("fa-solid", "fa-hand");
+        resultContent.innerHTML = "Gagné !";
+        playerScore = playerScore +1;
+        
+    }
+
+    if(computerSign == "Scissors"){
+        computerChooseElement.classList.add("fa-solid", "fa-hand-scissors");
+        resultContent.innerHTML = "Egalité !";
+    }
 })
 
 
